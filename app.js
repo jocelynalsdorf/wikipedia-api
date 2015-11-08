426ff3d130a0bb1e85cf067bad975a1247dcc147
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-07 16:08:39 -0800

    clean up indentation

--- app.js
+++ app.js
@@ -4,24 +4,19 @@ $(document).ready(function(){
     var x;
     var html = "";
     e.preventDefault();
+//get term to search wikipedia for
     var userInput = $("#userInfo").val();
 
- 
-
-   $.getJSON("https://en.wikipedia.org/w/api.php?format=json&callback=?&action=query&generator=search&gsrnamespace=0&gsrsearch=" + userInput + "&gsrlimit=20&prop=info&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max", function(data) {
-    x = data["query"].pages;
-
-  for(var prop in x) {
-    console.log(x[prop]);
-    html += "<li class='result-list-item'><h3 class='title'><a href='" +  x[prop].fullurl + "'>" + x[prop].title + " </a></h3></li>"
-    console.log(x[prop].title);
-  }
-   
+    $.getJSON("https://en.wikipedia.org/w/api.php?format=json&callback=?&action=query&generator=search&gsrnamespace=0&gsrsearch=" + userInput + "&gsrlimit=20&prop=info&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max", function(data) {
+      x = data["query"].pages;
+//get the data we need from the response
+        for(var prop in x) {
+          html += "<li class='result-list-item'><h3 class='title'><a href='" +  x[prop].fullurl + "'>" + x[prop].title + " </a></h3></li>"          
+        }
         
-
-  $(".message").html(html);
+//show content on view
+    $(".message").html(html);
     
-
-  });//end of get
+    });//end of get
   });//end of click
 });
