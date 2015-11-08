3ae47576c2013bc133d01e04455fb695ed7653fe
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-07 16:06:04 -0800

    link title to the corresponding url

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 
   $(".searchBtn").on("click", function(e){
     var x;
+    var html = "";
     e.preventDefault();
     var userInput = $("#userInfo").val();
 
- console.log(userInput);
+ 
 
-   $.getJSON("https://en.wikipedia.org/w/api.php?format=json&callback=?&action=query&generator=search&gsrnamespace=0&gsrsearch=" + userInput + "&gsrlimit=10&prop=info&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max", function(data) {
+   $.getJSON("https://en.wikipedia.org/w/api.php?format=json&callback=?&action=query&generator=search&gsrnamespace=0&gsrsearch=" + userInput + "&gsrlimit=20&prop=info&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max", function(data) {
     x = data["query"].pages;
-//console.log( JSON.stringify(data))
+
   for(var prop in x) {
     console.log(x[prop]);
+    html += "<li class='result-list-item'><h3 class='title'><a href='" +  x[prop].fullurl + "'>" + x[prop].title + " </a></h3></li>"
+    console.log(x[prop].title);
   }
    
         
-        
-//         $.each(data, function (i, obj) {
-//         console.log(obj["pages"]);
-// });
- //$(".message").html(html);
+
+  $(".message").html(html);
     
 
   });//end of get
